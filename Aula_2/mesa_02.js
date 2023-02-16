b733ea0754e5014f7515ac39b591f01f1a8b7484
felipe-2833 <69013950+felipe-2833@users.noreply.github.com>
2023-02-16 19:06:22 -0300

    mesa2

--- Aula_2/mesa_02.js
+++ Aula_2/mesa_02.js
@@ -1,12 +1,18 @@
+//mensagem para iniciar o jogo
 alert("Vamos jogar JOKENPO??\n Para ganhar o jogo é preciso ganhar 2 rodadas.")
+
+//declaração de variaveis
 let pontosJogador = 0;
 let pontosMaquina = 0;
+
+//função para estabelecer o numero gerado aleatoriamente pela maquina
 let aleatorio = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//função de comparação entre os valares: da maquina e do jogador 
 let jokenpo = (valorJogador,valorMaquina) => {
 
     switch (valorJogador){
@@ -50,6 +56,8 @@ let jokenpo = (valorJogador,valorMaquina) => {
         break;
     }
 }
+
+//loop que estabelece as regras da mesa: só ganha quem ganhar 2 rodadas
 while(pontosJogador < 2 && pontosMaquina < 2){
     let valorInformado = prompt("Informe um dos numeros inteiros:\n 1 = papel\n 2 = tesoura\n 3 = pedra");
     let valorConvertido = Number(valorInformado);
@@ -70,6 +78,7 @@ while(pontosJogador < 2 && pontosMaquina < 2){
     }
 }
 
+//mensagem para o ganhador 
 if (pontosJogador === 2){
     alert("PARABENS!!!\n Você ganhou!")
 }
