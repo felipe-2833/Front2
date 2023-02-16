03963a1c02bb98a6929f247380e255fd0cd4d968
felipe-2833 <69013950+felipe-2833@users.noreply.github.com>
2023-02-16 18:53:51 -0300

    mesa 2

---
+++ Aula_2/mesa_02.js
@@ -0,0 +1,87 @@
+alert("Vamos jogar JOKENPO??\n Para ganhar o jogo é preciso ganhar 2 rodadas.")
+let pontosJogador = 0;
+let pontosMaquina = 0;
+let aleatorio = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let jokenpo = (valorJogador,valorMaquina) => {
+
+    switch (valorJogador){
+        case 1:
+        if (valorMaquina === 3){
+          return true
+        }
+
+        else if (valorMaquina === 2){
+            return false
+        }
+
+        else if (valorMaquina === 1){
+            return 0
+        }
+
+        break;
+
+        case 2:
+        if (valorMaquina === 1){
+            return true
+        }
+        else if (valorMaquina === 3){
+            return false
+        }
+        else if (valorMaquina === 2){
+            return 0
+        }
+        break;
+
+        case 3:
+        if (valorMaquina === 2){
+            return true
+        }
+        else if (valorMaquina === 1){
+            return false
+        }
+        else if (valorMaquina === 3){
+            return 0
+        }
+        break;
+    }
+}
+while(pontosJogador < 2 && pontosMaquina < 2){
+    let valorInformado = prompt("Informe um dos numeros inteiros:\n 1 = papel\n 2 = tesoura\n 3 = pedra");
+    let valorConvertido = Number(valorInformado);
+    let valorAleatorio = aleatorio(1,3);
+    let ganhador = jokenpo(valorConvertido,valorAleatorio)
+
+    if (ganhador === true){
+        pontosJogador++
+        alert("Você ganhou esta rodada!\n Placar: ${pontosJogador}x${pontosMaquina}")
+    }
+    else if (ganhador === false){
+        pontosMaquina++
+        alert("Você perdeu esta rodada!\n Placar: ${pontosJogador}x${pontosMaquina}")
+
+    }
+    else if (ganhador === 0){
+        alert("Empate!\n Placar: ${pontosJogador}x${pontosMaquina}")
+    }
+}
+
+if (pontosJogador === 2){
+    alert("PARABENS!!!\n Você ganhou!")
+}
+
+else if (pontosMaquina === 2){
+    alert("AAAAAAAAAAH\n Você perdeu :(")
+}
+
+
+
+
+
+
+
+
